7745a524b0a47e537b337d19f91e6a9b87e5c890
yeremiaChris <yeremia997@gmail.com>
2024-05-03 07:58:46 +0700

    feat: remove unused code

--- src/modules/task/TaskCard.tsx
+++ src/modules/task/TaskCard.tsx
@@ -1,6 +1,6 @@
 import { Card, CardBody, CardProps } from "@nextui-org/react";
 import Status from "./Status";
-import { ReactNode, memo } from "react";
+import { ReactNode } from "react";
 
 export type StatusType = "Todo" | "Doing" | "Done" | "Delete";
 
@@ -11,19 +11,22 @@ export interface TaskCardProps extends CardProps {
   description: string;
 }
 
-const TaskCard = memo(
-  ({ description, children, title, date, ...taskProps }: TaskCardProps) => {
-    return (
-      <Card {...taskProps}>
-        <CardBody>
-          <Status>{children}</Status>
-          <h2 className="mt-2 mb-1">{title || "-"}</h2>
-          <p className="text-xs">{description || "-"}</p>
-          <p className="text-xs mt-2 text-gray-700">Due date: {date || "-"}</p>
-        </CardBody>
-      </Card>
-    );
-  }
-);
-
+const TaskCard = ({
+  description,
+  children,
+  title,
+  date,
+  ...taskProps
+}: TaskCardProps) => {
+  return (
+    <Card {...taskProps}>
+      <CardBody>
+        <Status>{children}</Status>
+        <h2 className="mt-2 mb-1">{title || "-"}</h2>
+        <p className="text-xs">{description || "-"}</p>
+        <p className="text-xs mt-2 text-gray-700">Due date: {date || "-"}</p>
+      </CardBody>
+    </Card>
+  );
+};
 export default TaskCard;
