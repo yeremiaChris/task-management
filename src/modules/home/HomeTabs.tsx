48790e194821561cbce93d89fb419a93579defdf
yeremiaChris <yeremia997@gmail.com>
2024-05-03 10:51:53 +0700

    feat: handle parent with useImperativeHandle for performance

--- src/modules/home/HomeTabs.tsx
+++ src/modules/home/HomeTabs.tsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import TaskCard, { StatusType, TaskCardProps } from "../task/TaskCard";
 import { FiTrash2, FiEdit } from "react-icons/fi";
-import { Button, useDisclosure } from "@nextui-org/react";
-import CreateTaskForm, { CreateTaskFormField } from "../task/CreateTaskForm";
+import { Button } from "@nextui-org/react";
+import CreateTaskForm, { CreateTaskFormRef } from "../task/CreateTaskForm";
 import { toast } from "react-toastify";
 export interface UpdateStatus {
   index: number;
@@ -11,10 +11,6 @@ export interface UpdateStatus {
 
 const HomeTabs = () => {
   const [tasks, setTask] = useState<TaskCardProps[]>([]);
-
-  const [detailTask, setDetailTask] = useState<CreateTaskFormField | null>(
-    null
-  );
 
   const createTask = (payload: TaskCardProps, index?: string) => {
     setTask((prevState) => {
@@ -28,17 +24,12 @@ const HomeTabs = () => {
     toast.success(`Success ${index ? "update" : "create"} task`);
   };
 
-  const disclosure = useDisclosure();
+  const createTaskFormRef = useRef<CreateTaskFormRef>(null);
 
   return (
     <>
       <div className="mt-4 grid gap-3">
-        <CreateTaskForm
-          handleCreateTask={createTask}
-          disclosureProps={disclosure}
-          defaultForm={detailTask}
-          resetDetail={() => setDetailTask(null)}
-        />
+        <CreateTaskForm handleCreateTask={createTask} ref={createTaskFormRef} />
 
         {!tasks.length ? (
           <p className="text-center my-5">No task to display.</p>
@@ -67,13 +58,12 @@ const HomeTabs = () => {
                     size="sm"
                     variant="bordered"
                     onClick={() => {
-                      setDetailTask({
-                        title: task.title,
-                        date: task.date as string,
+                      createTaskFormRef.current?.setDetail({
+                        date: task.date ?? "",
                         description: task.description,
+                        title: task.title,
                         index: index.toString(),
                       });
-                      disclosure.onOpen();
                     }}
                   >
                     <FiEdit />
--- src/modules/task/CreateTaskForm.tsx
+++ src/modules/task/CreateTaskForm.tsx
@@ -7,13 +7,13 @@ import {
   Button,
   Input,
   ButtonProps,
-  UseDisclosureProps,
   ModalProps,
   Textarea,
+  useDisclosure,
 } from "@nextui-org/react";
 import { useForm, SubmitHandler, Controller } from "react-hook-form";
 import { TaskCardProps } from "./TaskCard";
-import { useEffect } from "react";
+import { Ref, forwardRef, useImperativeHandle, useState } from "react";
 
 export type CreateTaskFormField = {
   title: string;
@@ -24,136 +24,137 @@ export type CreateTaskFormField = {
 
 interface CreateTaskForm extends Partial<ModalProps> {
   buttonProps?: ButtonProps;
-  disclosureProps: UseDisclosureProps & { onOpenChange?: () => void };
-  defaultForm?: CreateTaskFormField | null;
-  resetDetail: () => void;
   handleCreateTask: (props: TaskCardProps, index?: string) => void;
 }
 
-const CreateTaskForm = ({
-  defaultForm,
-  buttonProps,
-  disclosureProps,
-  resetDetail,
-  handleCreateTask,
-  ...modalProps
-}: CreateTaskForm) => {
-  const { isOpen, onOpen, onClose } = disclosureProps;
-
-  const { handleSubmit, control, reset, setValue } =
-    useForm<CreateTaskFormField>();
-
-  const onSubmit: SubmitHandler<CreateTaskFormField> = (
-    data: CreateTaskFormField
-  ) => {
-    handleCreateTask(
-      {
-        date: data.date,
-        description: data.description,
-        title: data.title,
-      },
-      defaultForm?.index
-    );
-    if (onClose) onClose();
-  };
-
-  useEffect(() => {
-    if (defaultForm) {
-      setValue("date", defaultForm.date);
-      setValue("title", defaultForm.title);
-      setValue("description", defaultForm.description);
-    }
-  }, [defaultForm, setValue]);
-
-  useEffect(() => {
-    if (!isOpen) {
-      resetDetail();
-      reset();
-    }
-  }, [isOpen, resetDetail, reset]);
-
-  return (
-    <>
-      <Button
-        onPress={onOpen}
-        color="primary"
-        {...buttonProps}
-        className="font-bold"
-      >
-        Create Task
-      </Button>
-      <Modal
-        isOpen={isOpen}
-        placement="auto"
-        as="form"
-        onSubmit={handleSubmit(onSubmit)}
-        hideCloseButton
-        {...modalProps}
-      >
-        <ModalContent>
-          <ModalHeader className="flex flex-col gap-1">
-            {defaultForm ? "Update" : "Create"} Task
-          </ModalHeader>
-          <ModalBody>
-            <Controller
-              control={control}
-              name="title"
-              defaultValue=""
-              render={({ field }) => (
-                <Input
-                  autoFocus
-                  label="Title"
-                  placeholder="Enter your title"
-                  id="title"
-                  variant="bordered"
-                  isRequired
-                  {...field}
-                />
-              )}
-            />
-
-            <Controller
-              control={control}
-              name="date"
-              defaultValue=""
-              render={({ field }) => (
-                <Input
-                  label="Due date"
-                  variant="bordered"
-                  id="date"
-                  type="date"
-                  {...field}
-                />
-              )}
-            />
-
-            <Controller
-              control={control}
-              name="description"
-              defaultValue=""
-              render={({ field }) => (
-                <Textarea
-                  id="description"
-                  label="Description"
-                  placeholder="Enter your description"
-                  variant="bordered"
-                  {...field}
-                />
-              )}
-            />
-          </ModalBody>
-          <ModalFooter>
-            <Button variant="bordered" type="button" onClick={onClose}>
-              Close
-            </Button>
-            <Button color="primary" type="submit">
-              {defaultForm ? "Update" : "Submit"}
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-    </>
-  );
+export type CreateTaskFormRef = {
+  setDetail: (payload: CreateTaskFormField) => void;
 };
+const CreateTaskForm = forwardRef(
+  (
+    { buttonProps, handleCreateTask, ...modalProps }: CreateTaskForm,
+    ref: Ref<CreateTaskFormRef>
+  ) => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const [detailTask, setDetailTask] = useState<CreateTaskFormField | null>(
+      null
+    );
+
+    const { handleSubmit, control, setValue, reset } =
+      useForm<CreateTaskFormField>();
+
+    const handleClose = () => {
+      onClose();
+      reset();
+    };
+
+    const onSubmit: SubmitHandler<CreateTaskFormField> = (
+      data: CreateTaskFormField
+    ) => {
+      handleCreateTask(
+        {
+          date: data.date,
+          description: data.description,
+          title: data.title,
+        },
+        detailTask?.index
+      );
+      if (onClose) onClose();
+    };
+
+    useImperativeHandle(ref, () => ({
+      setDetail: (payload: CreateTaskFormField) => {
+        onOpen();
+        setDetailTask(payload);
+        setValue("date", payload.date ?? "");
+        setValue("title", payload.title);
+        setValue("description", payload.description);
+      },
+    }));
+
+    return (
+      <>
+        <Button
+          onPress={onOpen}
+          color="primary"
+          {...buttonProps}
+          className="font-bold"
+        >
+          Create Task
+        </Button>
+        <Modal
+          isOpen={isOpen}
+          placement="auto"
+          as="form"
+          onSubmit={handleSubmit(onSubmit)}
+          hideCloseButton
+          {...modalProps}
+        >
+          <ModalContent>
+            <ModalHeader className="flex flex-col gap-1">
+              {detailTask ? "Update" : "Create"} Task
+            </ModalHeader>
+            <ModalBody>
+              <Controller
+                control={control}
+                name="title"
+                defaultValue=""
+                render={({ field }) => (
+                  <Input
+                    autoFocus
+                    label="Title"
+                    placeholder="Enter your title"
+                    id="title"
+                    variant="bordered"
+                    isRequired
+                    {...field}
+                  />
+                )}
+              />
+
+              <Controller
+                control={control}
+                name="date"
+                defaultValue=""
+                render={({ field }) => (
+                  <Input
+                    label="Due date"
+                    variant="bordered"
+                    id="date"
+                    type="date"
+                    {...field}
+                  />
+                )}
+              />
+
+              <Controller
+                control={control}
+                name="description"
+                defaultValue=""
+                render={({ field }) => (
+                  <Textarea
+                    id="description"
+                    label="Description"
+                    placeholder="Enter your description"
+                    variant="bordered"
+                    {...field}
+                  />
+                )}
+              />
+            </ModalBody>
+            <ModalFooter>
+              <Button variant="bordered" type="button" onClick={handleClose}>
+                Close
+              </Button>
+              <Button color="primary" type="submit">
+                {detailTask ? "Update" : "Submit"}
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </>
+    );
+  }
+);
 
 export default CreateTaskForm;
